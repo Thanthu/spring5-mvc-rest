722727a2f5e681154cbe67259b56b82ef8ea5a1f
Thanthu <tnair@orthofx.com>
2021-08-22 17:38:43 +0530

    Create Category Entity, DTO and Repository - closes #2. Add Bootstrap data - closes #1.

---
+++ src/main/java/com/thanthu/springmvcrest/model/CategoryDTO.java
@@ -0,0 +1,13 @@
+package com.thanthu.springmvcrest.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryDTO {
+
+	private Long id;
+	private String name;
+
+}
---
+++ src/main/java/com/thanthu/springmvcrest/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.thanthu.springmvcrest.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.thanthu.springmvcrest.domain.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
